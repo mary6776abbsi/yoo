cebfb6c0f10bc64c564bca4bb6a2cdab83b7e313
maryam abbasi <169179074+mary6776abbsi@users.noreply.github.com>
2024-06-12 20:01:30 +0330

    yoo

---
+++ yoo.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool isPrime(int number) {
+    // بررسی اول بودن عدد
+    if (number <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= number; ++i) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int number;
+
+    // ورودی از کاربر گرفته می‌شود
+    cout << "Enter a positive integer: ";
+    cin >> number;
+
+    // بررسی آیا عدد اول است یا خیر و چاپ نتیجه
+    if (isPrime(number)) {
+        cout << number << " is a prime number." << endl;
+    }
+    else {
+        cout << number << " is not a prime number." << endl;
+    }
+
+    return 0;
+}
